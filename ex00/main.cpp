f7d23a726ed5f1b82f2afe8d8ff5b0bee7f133b8
Anouk Fleur <afleur@student.codam.nl>
2026-08-17 16:55:51 +0200

    ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -8,8 +8,16 @@ int	main()
 	{
 		big_chungus.incGrade();
 	}
+	catch (Bureaucrat::GradeTooLowException const & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	catch (Bureaucrat::GradeTooHighException const & e)
 	{
 		std::cout << e.what() << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "This error has not been otherwise categorised." << std::endl;
 	}
 }
