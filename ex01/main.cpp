ca0d942028d36f424a49fab586db1642ce77a210
Anouk Fleur <afleur@student.codam.nl>
2026-09-22 18:06:39 +0200

    Finished ex01 which includes updating the Makefile.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,23 +1,17 @@
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
+#include <iostream>
 
 int	main()
 {
 	Bureaucrat	big_chungus("Big Chungus", 1);
+	Bureaucrat	medium_chungus("Medium Chungus", 10);
+	Form		spellcheck("Automated Spellchecker-checker update 42.69", 1, 150);
 
-	try
-	{
-		big_chungus.incGrade();
-	}
-	catch (Bureaucrat::GradeTooLowException const & e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	catch (Bureaucrat::GradeTooHighException const & e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	catch (...)
-	{
-		std::cout << "This error has not been otherwise categorised." << std::endl;
-	}
+	std::cout << big_chungus << '\n' << medium_chungus << '\n' << spellcheck << std::endl;
+
+	medium_chungus.signForm(spellcheck);
+	std::cout << spellcheck << std::endl;
+	big_chungus.signForm(spellcheck);
+	std::cout << spellcheck << std::endl;
 }
